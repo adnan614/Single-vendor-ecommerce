383a7ae2cea6062ea2fd433760ef835b2a76e83a
Moulin Talukder <65370778+Moulin-Talukder@users.noreply.github.com>
2021-03-30 04:13:09 +0600

    product add view and file handling

--- routes/web.php
+++ routes/web.php
@@ -72,13 +72,6 @@ Route::get('/backend/brand/update/{id}',[BrandsController::class, 'brandUpdate']
 
 
 //category
-<<<<<<< HEAD
-Route::get('/category/category_form/', [CategoryController::class, 'categoryForm'])->name('category.form');
-Route::post('/category/category_add/', [CategoryController::class, 'categoryAdd'])->name('category.add');
-Route::get('/category/category_list/', [CategoryController::class, 'categoryList'])->name('category.list');
-
-Route::get('/slider/slider_form/',[SliderController::class,'sliderForm'])->name('slider.show');
-=======
 Route::get('/category/category_form/', [CategoriesController::class, 'categoryForm'])->name('category.form');
 Route::post('/category/category_add/', [CategoriesController::class, 'categoryAdd'])->name('category.add');
 Route::get('/category/category_list/', [CategoriesController::class, 'categoryList'])->name('category.list');
@@ -97,4 +90,7 @@ Route::post('/user/store', [UserController::class, 'userstore'])->name('user.sto
 //Customer Login
 Route::get('/user/log', [Login::class, 'userlog'])->name('user.loginform');
 Route::post('/user/login', [LoginController::class, 'loginput'])->name('user.login');
->>>>>>> d920eed3b2d6a3353698c2be0b7d4dcb909972ea
+
+
+//slider
+Route::get('/slider/slider_form/',[SliderController::class,'sliderForm'])->name('slider.show');
