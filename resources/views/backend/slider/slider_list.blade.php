01c4eec1dd435e3df50feb2866a5486bd6962eca
Adnan Faruque Alvi <adnanfaruque23@gmail.com>
2021-03-29 18:44:11 +0600

    sidebar

--- resources/views/backend/slider/slider_list.blade.php
+++ resources/views/backend/slider/slider_list.blade.php
@@ -12,11 +12,11 @@
                                         <br>
                                         <br>
 
-
-                                        <a href="#" style="float:right; margin-right: 10px;" class="btn btn-danger">
-                                                +Add Slider
-
-                                        </a>
+                                        <form method="get" action="#">
+                                                <button class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" style="float:right; margin-right:10px;" type="submit">
+                                                        Add Slider
+                                                </button>
+                                        </form>
                                 </div>
                         </div>
                 </div>
